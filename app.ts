e7b50b4e2b0ccea945330faf5559d4ab79d6e455
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-31 00:40:14 +0200

    refact: remove dotenv from app

--- app.ts
+++ app.ts
@@ -1,10 +1,14 @@
 import express from "express"
 import dotenv from "dotenv"
-import sequelize from "./utils/database-connection"
-// init env vars
-dotenv.config()
+import sequelize, {connectionInfo} from "./utils/database-connection"
 
 const app = express()
 
+const PORT =  process.env.PORT || 3333
 // start listening on 3333
-app.listen(process.env.PORT || 3333,()=>{ console.log("listen on 3333...") })
+app.listen( PORT , async () => {
+    console.log(`username ${connectionInfo.username} password ${connectionInfo.password}`)
+    console.log(`listening on port ${PORT} @ ${connectionInfo.host}`);
+    await sequelize.authenticate()
+    console.log('database connected successfully');
+})
--- utils/database-connection.ts
+++ utils/database-connection.ts
@@ -1,16 +1,16 @@
 import { Sequelize } from "sequelize"
 
-const credentials = {
+export const connectionInfo = {
     database: process.env.DATABASE_NAME || 'leon',
     username: process.env.USERNAME || 'leon',
     password: process.env.PASSWORD  || 'leon',
+    host: process.env.HOST || 'localhost',
+    port: parseInt(process.env.DATABASE_PORT||"") || 3306
 }
 
-const sequelize = new Sequelize(credentials.database,
-    credentials.username,
-    credentials.password,
+const sequelize = new Sequelize(
     {
-        host: process.env.HOST,
+        ...connectionInfo,
         dialect: 'mysql'
     }
 );
