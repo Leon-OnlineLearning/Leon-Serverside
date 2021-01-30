eb259930e21bb773d0b77b2b4a94dfbf69d6cf60
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-01-31 00:06:57 +0200

    feat: create db connection

--- utils/database-connection.ts
+++ utils/database-connection.ts
@@ -1,1 +1,18 @@
 import { Sequelize } from "sequelize"
+
+const credentials = {
+    database: process.env.DATABASE_NAME === undefined ? 'leon' : process.env.DATABASE_NAME,
+    username: process.env.USERNAME === undefined ? 'leon' : process.env.USERNAME,
+    password: process.env.PASSWORD === undefined ? 'leon' : process.env.PASSWORD,
+}
+
+const sequelize = new Sequelize(credentials.database,
+    credentials.username,
+    credentials.password,
+    {
+        host: process.env.HOST,
+        dialect: 'mysql'
+    }
+);
+
+export default sequelize;
