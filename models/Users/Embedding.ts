2d1f86acec6020d979c797dec2ffb7321cef6803
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2021-04-22 18:08:01 +0200

    fix: configure embedding vector

--- models/Users/Embedding.ts
+++ models/Users/Embedding.ts
@@ -9,7 +9,7 @@ import Student from "./Student";
 
 @Entity()
 export default class Embedding {
-  @Column("decimal", { array: true, precision: 11, scale: 10 })
+  @Column("decimal", { array: true, precision: 21, scale: 20 })
   vector: number[];
 
   @PrimaryGeneratedColumn("uuid")
