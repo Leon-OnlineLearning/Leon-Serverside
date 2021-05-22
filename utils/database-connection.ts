baebaa1f6dfe832864c7f18d9d41a04f3d5981ad
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-05-22 08:11:53 +0200

    feat: add base professr

--- utils/database-connection.ts
+++ utils/database-connection.ts
@@ -1,8 +1,11 @@
 import AdminLogic from "@controller/BusinessLogic/User/Admin/admin-logic";
 import AdminLogicImpl from "@controller/BusinessLogic/User/Admin/admin-logic-impl";
+import ProfessorLogic from "@controller/BusinessLogic/User/Professor/professors-logic";
+import ProfessorLogicIml from "@controller/BusinessLogic/User/Professor/professors-logic-impl";
 import StudentLogic from "@controller/BusinessLogic/User/Student/students-logic";
 import StudentLogicImpl from "@controller/BusinessLogic/User/Student/students-logic-impl";
 import Admin from "@models/Users/Admin";
+import Professor from "@models/Users/Professor";
 import Student from "@models/Users/Student";
 import {
     Connection,
@@ -44,6 +47,20 @@ export const initializeDBMSConnection = async () => {
         baseStudent.thirdPartyAccount = false;
 
         await studentlogic.createStudent(baseStudent);
+    }
+    if (
+        typeof process.env.BASE_PROFESSOR_EMAIL === "string" &&
+        typeof process.env.BASE_PROFESSOR_PASSWORD === "string"
+    ) {
+        const professorLogic: ProfessorLogic = new ProfessorLogicIml();
+        const professor = new Professor();
+        professor.email = process.env.BASE_PROFESSOR_EMAIL;
+        professor.password = process.env.BASE_PROFESSOR_PASSWORD;
+        professor.firstName = "base professor fn";
+        professor.lastName = "base professor ln";
+        professor.thirdPartyAccount = false;
+
+        await professorLogic.createProfessor(professor);
     }
 };
 
