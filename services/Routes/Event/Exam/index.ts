a2614dd1fcee2294e90a3cba2156aafbb7d6dcf0
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2021-05-28 00:38:29 +0200

    refact: seprate reporting function

--- services/Routes/Event/Exam/index.ts
+++ services/Routes/Event/Exam/index.ts
@@ -24,6 +24,7 @@ import StudentLogic from "@controller/BusinessLogic/User/Student/students-logic"
 import Embedding from "@models/Users/Embedding";
 import ReportLogic from "@controller/BusinessLogic/Report/report-logic";
 import { ReportLogicImpl } from "@controller/BusinessLogic/Report/report-logic-impl";
+import { report_res } from "./send_utils";
 
 const ffmpegPath = require("@ffmpeg-installer/ffmpeg").path;
 const ffmpeg = require("fluent-ffmpeg");
@@ -38,6 +39,8 @@ const parser: BodyParserMiddleware = new ExamParser();
 
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
+
+const serverBaseUrl = `${process.env.ML_SO_IO_SERVER_BASE_D}:${process.env.ML_SO_IO_SERVER_PORT}`;
 
 /**
  * save exam recording
@@ -90,52 +93,6 @@ router.put(
             const randNum = Date.now() + req.body.userId;
             const chunkPath = `/tmp/chunk_${randNum}.mkv`;
 
-            const send_file = async () => {
-                const result_callback: ExamChunkResultCallback = async (
-                    userId: string,
-                    examId: string,
-                    res: string,
-                    chunkStartTime: number,
-                    chunkEndTime: number
-                ) => {
-                    const matching = res;
-                    console.log(matching);
-
-                    if (!matching) {
-                        // TODO save database
-                        console.log("will save");
-                        const reportlogic: ReportLogic = new ReportLogicImpl();
-                        reportlogic.addToReport(
-                            userId,
-                            examId,
-                            chunkStartTime,
-                            chunkEndTime - chunkStartTime
-                        );
-                        console.log(chunkStartTime);
-                        console.log(res);
-                    }
-                };
-
-                // send the file and delete it
-                const serverBaseUrl = `${process.env.ML_SO_IO_SERVER_BASE_D}:${process.env.ML_SO_IO_SERVER_PORT}`;
-
-                const studentLogic: StudentLogic = new StudentLogicImpl();
-
-                const embedding: Embedding = await studentLogic.getEmbedding(
-                    req.body.userId
-                );
-
-                sendExamFile(
-                    req.body.userId,
-                    serverBaseUrl,
-                    fileInfo,
-                    chunkPath,
-                    embedding,
-                    result_callback
-                );
-                // * save time stamp if not match
-            };
-
             ffmpeg(filePath)
                 .setStartTime(fileInfo.chunkStartTime)
                 .setDuration(10)
@@ -145,7 +102,14 @@ router.put(
                 .on("end", async function (err: any) {
                     if (!err) {
                         console.log("conversion Done");
-                        await send_file();
+                        await sendExamFile(
+                            req.body.userId,
+                            serverBaseUrl,
+                            fileInfo,
+                            chunkPath,
+                            embedding,
+                            report_res
+                        );
                     }
                 })
                 .on("error", function (err: any) {
