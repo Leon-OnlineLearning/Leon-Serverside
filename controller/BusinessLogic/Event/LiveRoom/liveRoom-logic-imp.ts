c2f917bc34cee8a768602d35920763504f20fb65
abdo hashem <ar1813@fayoum.edu.eg>
2021-06-30 14:55:50 +0200

    feat: implement room creation janus

---
+++ controller/BusinessLogic/Event/LiveRoom/liveRoom-logic-imp.ts
@@ -0,0 +1,57 @@
+import AudioRoom from "@models/Events/AudioRoom";
+import Lecture from "@models/Events/Lecture/Lecture";
+import UserTypes from "@models/Users/UserTypes";
+import UserInputError from "@services/utils/UserInputError";
+import { getRepository } from "typeorm";
+import LecturesLogicImpl from "../Lecture/lectures-logic-impl";
+import LiveRoomLogic from "./liveRoom-logic";
+import { start_janus_room } from "./utils";
+
+const janus_server =
+    process.env.janus_server || "http://janus-gateway:8088/janus";
+
+export default class LiveRoomLogicImpl implements LiveRoomLogic {
+    async enter_lecture_room(
+        lectureId: string,
+        userRole: UserTypes
+    ): Promise<AudioRoom> {
+        let lecture = await new LecturesLogicImpl().getLectureById(lectureId);
+        let liveRoom = await this.getRoomByLectureId(lectureId);
+
+        if (liveRoom.isAlive) {
+            return liveRoom;
+        }
+
+        const isLectureTime =
+            lecture.startTime < new Date() && lecture.endTime > new Date();
+        if (!isLectureTime) {
+            throw new Error("lecture is not now");
+        }
+
+        const canStartRoom = userRole == UserTypes.PROFESSOR && isLectureTime;
+        if (!canStartRoom) {
+            throw new Error("wait the professor to start lecture");
+        }
+        const file_path = "lecture/req.params.lectureId.wav"; //REVIEW
+
+        await start_janus_room(
+            liveRoom.roomId,
+            janus_server,
+            liveRoom.roomSecret,
+            "notImplemented",
+            file_path
+        );
+
+        liveRoom.isAlive = true;
+        getRepository(AudioRoom).save(liveRoom);
+        return liveRoom;
+    }
+
+    async getRoomByLectureId(lectureId: string) {
+        const lecture = await getRepository(Lecture).findOne(lectureId, {
+            relations: ["liveRoom"],
+        });
+        if (lecture) return lecture.liveRoom;
+        else throw new UserInputError("Invalid lecture id");
+    }
+}
