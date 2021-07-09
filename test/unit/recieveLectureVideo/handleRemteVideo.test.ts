d3551c8b32effa96e3c8665990652e6867ea835c
abdo hashem <ar1813@fayoum.edu.eg>
2021-07-09 17:10:33 +0200

    skip test propably

--- test/unit/recieveLectureVideo/handleRemteVideo.test.ts
+++ test/unit/recieveLectureVideo/handleRemteVideo.test.ts
@@ -1,20 +1,16 @@
-// import LecturesLogicImpl from "@controller/BusinessLogic/Event/Lecture/lectures-logic-impl";
+import LecturesLogicImpl from "@controller/BusinessLogic/Event/Lecture/lectures-logic-impl";
 
 describe("handling recording at remote janus server", () => {
-    it("should pass", async () => {
-        // this test require live server to be running so i disabled it here
-        // to pass PR check
+    it.skip("should list available lectures at remote server", async () => {
+        const remote_files = await new LecturesLogicImpl().listRemoteRecordings();
+        expect(remote_files.length > 1);
     });
-    //     it("should list available lectures at remote server", async () => {
-    //         const remote_files = await new LecturesLogicImpl().listRemoteRecordings();
-    //         expect(remote_files.length > 1);
-    //     });
 
-    //     it("should save video and return a path for it", async () => {
-    //         const lectureLogic = new LecturesLogicImpl();
-    //         const remote_files = await lectureLogic.listRemoteRecordings();
-    //         const uuid = remote_files[0];
+    it.skip("should save video and return a path for it", async () => {
+        const lectureLogic = new LecturesLogicImpl();
+        const remote_files = await lectureLogic.listRemoteRecordings();
+        const uuid = remote_files[0];
 
-    //         expect(await lectureLogic.getRemoteRecording(uuid));
-    //     });
+        expect(await lectureLogic.getRemoteRecording(uuid));
+    });
 });
