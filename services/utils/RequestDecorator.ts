aa8249c9424bc30425c09e1ad9443838f72c3fdd
A-Siam <abdullah.khaled.kamal0@gmail.com>
2021-07-13 20:51:58 +0200

    fix: typo

--- services/utils/RequestDecorator.ts
+++ services/utils/RequestDecorator.ts
@@ -5,14 +5,15 @@ import { Response } from "express";
  * response format
  *
  * NOTE: notice the **simple** in the function name. This function is only intended
- * for simple final middlewares that deson't contain various reponse codes if this is
- * not you case please don't use this function to avoid unexpected results.
- *
- * @param res the response object of the last request in the middlewares queue
- * @param responseLogic the special logic that will happen inside the
+ * for simple final middleware that doesn't contain various response codes if this is
+ * not your case please don't use this function to avoid unexpected results.
+ * 
  * if responseLogic return a value it will return it as it is otherwise it
  * will return {success:true}, in case of exception will return
  * {success: false, message: error.message}
+ *
+ * @param res the response object of the last request in the middlewares queue
+ * @param responseLogic the special logic that will happen inside the
  */
 const simpleFinalMWDecorator = async (
     response: Response,
