ec2050c680bc7d819f3c2df1dc37c33293a7d598
Abdullah khaled <34966791+A-Siam@users.noreply.github.com>
2021-08-13 06:21:23 +0200

    Update and rename create_migrate.sh to create_migration.sh

--- create_migration.sh
+++ create_migration.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -ne 1 ]; then
     echo "Illegal number of parameters"
-    echo "Usage create_migtations migration_name"
+    echo "Usage create_migration.sh migration_name"
 else
     npx ts-node -r tsconfig-paths/register ./node_modules/.bin/typeorm migration:generate -n $1
 fi
